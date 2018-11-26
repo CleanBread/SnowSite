955cf17b3d91faf832312d69405b3370d7936f70
Gleb Chistyakov <gleb4ist@gmail.com>
2018-11-26 15:07:06 +0300

    закончены действия для header при прокрутке

--- js/header.js
+++ js/header.js
@@ -3,16 +3,27 @@
     var header = document.querySelector('.header');
     var logo = document.querySelector('.logo__img');
     var logoDark = document.querySelector('.logo__img_active');
+    var links = document.querySelectorAll('.navbar__link');
 
     window.onscroll = () => {
-        if (window.pageYOffset > 50) {
-            header.classList.add('header_active');
-            logo.classList.add('hide');
-            logoDark.classList.remove('hide');
-        } else {
-            header.classList.remove('header_active');
-            logo.classList.remove('hide');
-            logoDark.classList.add('hide');
+        var delay = () => {
+            if (window.pageYOffset > 50) {
+                header.classList.add('header_active');
+                logo.classList.add('hide');
+                logoDark.classList.remove('hide');
+                links.forEach((item) => {
+                    item.classList.add('navbar__link_active');
+                })
+            } else {
+                header.classList.remove('header_active');
+                logoDark.classList.add('hide');
+                logo.classList.remove('hide');
+                links.forEach((item) => {
+                    item.classList.remove('navbar__link_active');
+                })
+            }
         }
+        setTimeout(delay, 200);
     }
+
 })();
