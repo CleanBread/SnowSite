66760abc73cff3bfbeb951d59b0b0080c42a8813
Gleb Chistyakov <gleb4ist@gmail.com>
2018-11-26 17:20:25 +0300

    доделан блок about

--- js/header.js
+++ js/header.js
@@ -5,25 +5,31 @@
     var logoDark = document.querySelector('.logo__img_active');
     var links = document.querySelectorAll('.navbar__link');
 
-    window.onscroll = () => {
-        var delay = () => {
-            if (window.pageYOffset > 50) {
-                header.classList.add('header_active');
-                logo.classList.add('hide');
-                logoDark.classList.remove('hide');
-                links.forEach((item) => {
-                    item.classList.add('navbar__link_active');
-                })
-            } else {
-                header.classList.remove('header_active');
-                logoDark.classList.add('hide');
-                logo.classList.remove('hide');
-                links.forEach((item) => {
-                    item.classList.remove('navbar__link_active');
-                })
-            }
+    var header_active = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+            logo.classList.add('hide');
+            logoDark.classList.remove('hide');
+            links.forEach((item) => {
+                item.classList.add('navbar__link_active');
+            })
+        } else {
+            header.classList.remove('header_active');
+            logoDark.classList.add('hide');
+            logo.classList.remove('hide');
+            links.forEach((item) => {
+                item.classList.remove('navbar__link_active');
+            })
         }
-        setTimeout(delay, 200);
     }
+
+    window.onscroll = () => {
+        setTimeout(header_active, 200);
+    }
+
+    window.onload = () => {
+        setTimeout(header_active, 200);
+    }
+    
 
 })();
