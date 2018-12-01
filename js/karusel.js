950fd96b605d8b34b738d607ccff50953a81b845
Gleb Chistyakov <gleb4ist@gmail.com>
2018-12-01 23:44:18 +0300

    эта ебучая карусель почти закончена, сидел над ней долбанных четыре дня

--- js/karusel.js
+++ js/karusel.js
@@ -1,9 +1,12 @@
 (function () {
-    var item = document.querySelector(".box_absolute");
+    var box = document.querySelectorAll(".box_absolute");
+    var windowWidht = document.body.clientWidth;
 
-    var onItemDown = (evt) => {
+    var onBoxDown = (evt) => {
         evt.preventDefault();
         var target = evt.currentTarget;
+        var next = target.nextElementSibling;
+        var previous = target.previousElementSibling;
 
         var startCoords = {
             x: evt.clientX
@@ -20,21 +23,61 @@
             };
         
             var newLeftOffset = target.offsetLeft - shift.x;
-            target.style.left = (newLeftOffset) + 'px';
+            newLeftOffset = newLeftOffset * 100 / windowWidht;
+
+            target.style.left = newLeftOffset + '%';
+
+            if(next)
+            next.style.left = newLeftOffset + 100 + "%";
+
+            if(previous)
+            previous.style.left = newLeftOffset + -100 + "%";
+            
+            target.classList.remove('box_active');
+
+            if(next)
+            next.classList.remove('box_active');
+            if(previous)
+            previous.classList.remove('box_active');
         };
 
         var onMouseUp = function (upEvt) {
             upEvt.preventDefault();
             document.removeEventListener('mousemove', onMouseMove);
-            target.style.left = 0 + 'px';
-            target.classList.add('box_active');
+            if (target.offsetLeft <= -100 && next) {
+                target.style.left = -100 + '%';
+                target.classList.add('box_active');
+                if(next) {
+                    next.style.left = 0 + '%';
+                    next.classList.add('box_active');
+                }
+            } else if (target.offsetLeft >= 100 && previous) {
+                target.style.left = 100 + '%';
+                target.classList.add('box_active');
+                if(previous) {
+                    previous.style.left = 0 + '%';
+                    previous.classList.add('box_active');
+                }
+            } else {
+                target.style.left = 0 + '%';
+                target.classList.add('box_active');
+                if (next) {
+                    next.style.left = 100 + '%';
+                    next.classList.add('box_active');
+                }
+                if (previous) {
+                    previous.style.left = -100 + '%';
+                    previous.classList.add('box_active');
+                }
+            }
             document.removeEventListener('mouseup', onMouseUp);
         };
         
-        target.classList.remove('box_active');
         
         document.addEventListener('mousemove', onMouseMove);
         document.addEventListener('mouseup', onMouseUp);
     };
-    item.addEventListener("mousedown", onItemDown);
+    box.forEach((it) => {
+        it.addEventListener("mousedown", onBoxDown);
+    })
 })();
