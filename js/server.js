8baf527db72c6e97376733097d9d30f5e2f5fff1
Gleb <gleb4ist@gmail.com>
2019-01-02 01:29:43 +0500

    подключение к базе данных, отправка данных из нее и установка пакетов

--- js/server.js
+++ js/server.js
@@ -1,26 +1,28 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
-
-var mass = [
-    {
-        headline: 'Video Project',
-        text1: 'Nullam lobortis neque turpis, nec tempus sem pharetra at. Donec et quam, ullamcorper velit. Aliquam maximus ullamcorper ligula, at placerat dui hendrerit sed. Sed metus urna, bibendum id tortor, feugiat ipsum. Aliquam vehicula neque sit amet dolor malesuada pretium.',
-        text2: 'Curabitur tristique, felis ut mattis auctor, elit ante laoreet libero, ac lorem quam vitae libero. Suspen disse aliquet eget risus quis vehicula.',
-        image: '../images/portfolio-4-video-thumb.jpg',
-        client: 'Anna Doe',
-        date: '	06.20.2016',
-        share: 'Facebook, Twitter, Pinterest'
-    }
-];
-
-
+var db;
 
 app.get('/port', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    res.send(JSON.stringify(mass));
+
+    db.collection('modalWindow').find().toArray((err, docs) => {
+        if(err) {
+            console.log('error data')
+            return res.sendStatus(500);
+        }
+        res.send(JSON.stringify(docs))
+    });
 });
 
-app.listen(3000, '192.168.1.8');
+MongoClient.connect('mongodb://localhost:27017/modalWindow', (err, database) => {
+    if(err) {
+        return console.log('error database');
+    }
+    db = database;
+    app.listen(3000, '192.168.1.8');
+});
+
 // 192.168.0.13
